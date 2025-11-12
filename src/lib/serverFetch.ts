e8d7d5a402fdc1525ccbb282ed4cc87a5659eef7
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-11-13 02:51:15 +0600

    completed fetch helpter for server fetcing

---
+++ src/lib/serverFetch.ts
@@ -0,0 +1,38 @@
+import { getCookie } from "./tokenHandler"
+
+
+const serverFetchHelper = async (endPoint:string, options:RequestInit ):Promise<Response>=>{
+    const {headers, ...restOpions} = options
+    const accessToken = await getCookie("accessToken")
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL as string}${endPoint}`, {
+      headers:{
+        ...headers
+      },
+      ...(accessToken ? {"Authorization": accessToken} : {}),
+      ...restOpions
+    });
+
+    return res
+}
+
+
+export const serverFetch = {
+    get: async(endPoint:string, options:RequestInit = {}):Promise<Response>=> (serverFetchHelper(endPoint,{ ...options, method:"GET"})),
+
+    post: async(endPoint:string, options:RequestInit = {}):Promise<Response>=> (serverFetchHelper(endPoint,{ ...options, method:"POST"})),
+
+    patch: async(endPoint:string, options:RequestInit = {}):Promise<Response>=> (serverFetchHelper(endPoint,{ ...options, method:"PATCH"})),
+
+    put: async(endPoint:string, options:RequestInit = {}):Promise<Response>=> (serverFetchHelper(endPoint,{ ...options, method:"PUT"})),
+    
+    delete: async(endPoint:string, options:RequestInit = {}):Promise<Response>=> (serverFetchHelper(endPoint,{ ...options, method:"DELETE"})),
+}
+
+
+
+// method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(validatedFeild.data),
+//       credentials: "include",
