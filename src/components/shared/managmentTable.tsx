c13b69ed5e447ce43d15ac0d757c95fae5822ed1
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-11-13 18:04:41 +0600

    added managment tbale component for reusable

---
+++ src/components/shared/managmentTable.tsx
@@ -0,0 +1,145 @@
+import { Edit, Eye, Loader2, MoreHorizontal, Trash } from "lucide-react";
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+
+export interface Column<T> {
+  header: string;
+  accessor: keyof T | ((row: T) => React.ReactNode);
+  className?: string;
+}
+
+export interface ManagmentTAbleProps<T> {
+  data: T[];
+  columns: Column<T>[];
+  onView?: (row: T) => void;
+  onEdit?: (row: T) => void;
+  onDelete?: (row: T) => void;
+  getRowKey: (row: T) => string;
+  emptyMessage?: string;
+  isRefreshing?: boolean;
+}
+
+function ManagmentTable<T>({
+  data = [],
+  columns = [],
+  onView,
+  onEdit,
+  onDelete,
+  getRowKey,
+  emptyMessage = "No records found",
+  isRefreshing = false,
+}: ManagmentTAbleProps<T>) {
+  const hasActions = onDelete || onEdit || onView;
+  return (
+    <>
+      <div className="rounded-lg border relative">
+        {isRefreshing && (
+          <div className="absolute inset-0 bg-background/50 backdrop-blur-[2px] flex items-center justify-center z-10 rounded-1g">
+            <div className="flex flex-col items-center gap-2">
+              <Loader2 className="h-6 w-6 animate-spin text-primary" />
+              <p className="text-sm text-muted-foreground">Refreshing...</p>
+            </div>
+          </div>
+        )}
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {columns?.map((column, ind) => {
+                return (
+                  <TableCell key={ind} className={column?.className}>
+                    {column?.header}
+                  </TableCell>
+                );
+              })}
+              {hasActions && (
+                <TableHead className="w-[70px]">Actions</TableHead>
+              )}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.length === 0 ? (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length + (hasActions ? 1 : 0)}
+                  className="text-center py-8 text-muted-foreground"
+                >
+                  {emptyMessage}
+                </TableCell>
+              </TableRow>
+            ) : (
+              data?.map((item) => {
+                return (
+                  <TableRow key={getRowKey(item)}>
+                    {columns?.map((col, ind) => {
+                      return (
+                        <TableCell key={ind}>
+                          {typeof col.accessor === "function"
+                            ? col.accessor(item)
+                            : String(item[col.accessor])}
+                        </TableCell>
+                      );
+                    })}
+                    {hasActions && (
+                      <TableCell>
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <Button
+                              variant="ghost"
+                              size="icon"
+                              className="cursor-pointer"
+                            >
+                              <MoreHorizontal className="h-4 w-4" />
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent align="end">
+                            {onView && (
+                              <DropdownMenuItem onClick={() => onView(item)}>
+                                <Eye className="mr-2 h-4 w-4 cursor-pointer" />
+                                View
+                              </DropdownMenuItem>
+                            )}
+                            {onEdit && (
+                              <DropdownMenuItem onClick={() => onEdit(item)}>
+                                <Edit className="mr-2 h-4 w-4 cursor-pointer" />
+                                Edit
+                              </DropdownMenuItem>
+                            )}
+                            {onDelete && (
+                              <DropdownMenuItem
+                                onClick={() => onDelete(item)}
+                                className="text-destructive"
+                              >
+                                <Trash className="mr-2 h-4 w-4 cursor-pointer" />
+                                Delete
+                              </DropdownMenuItem>
+                            )}
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </TableCell>
+                    )}
+                  </TableRow>
+                );
+              })
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </>
+  );
+}
+
+export default ManagmentTable;
