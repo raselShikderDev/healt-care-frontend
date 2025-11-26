49b0cbf6ed99f1df50308dac68dd0ff93ac7286e
Rasel shikder <rasel.sikder777.rk@gmail.com>
2025-11-26 07:33:50 +0600

    completed doctor schedule page

--- src/services/doctor/doctorSchedules.ts
+++ src/services/doctor/doctorSchedules.ts
@@ -1,87 +1,90 @@
+'use server'
+
 import { serverFetch } from "@/lib/serverFetch";
 
-export async function getDoctorOwnSchedules(queryString: any) { 
- 
-  try {
-    const res = await serverFetch.get(`/doctor-schedule/my-schedules${queryString ? `?${queryString}` :""}`);
-    const data = await res.json();
 
-   
-  } catch (error: any) {
-    console.error(error);
-    return {
-      success: false,
-      message: `${process.env.NODE_ENV === "development"
-        ? error.message
-        : "Something went wrong"
-        }`,
-    };
-  }
+export async function getDoctorOwnSchedules(queryString: any) {
+
+    try {
+        const res = await serverFetch.get(`/doctor-schedule/my-schedules${queryString ? `?${queryString}` : ""}`);
+        const data = await res.json();
+
+return data
+    } catch (error: any) {
+        console.error(error);
+        return {
+            success: false,
+            message: `${process.env.NODE_ENV === "development"
+                ? error.message
+                : "Something went wrong"
+                }`,
+        };
+    }
 };
 
-export async function getAvailableSchedules() { 
- 
-  try {
-    const res = await serverFetch.get(`/schedule`);
-    const data = await res.json();
+export async function getAvailableSchedules() {
 
-   
-  } catch (error: any) {
-    console.error(error);
-    return {
-      success: false,
-      message: `${process.env.NODE_ENV === "development"
-        ? error.message
-        : "Something went wrong"
-        }`,
-    };
-  }
+    try {
+        const res = await serverFetch.get(`/schedule`);
+        const data = await res.json();
+        return data
+
+    } catch (error: any) {
+        console.error(error);
+        return {
+            success: false,
+            message: `${process.env.NODE_ENV === "development"
+                ? error.message
+                : "Something went wrong"
+                }`,
+        };
+    }
 };
 
 
-export async function createDoctorschedue(scheduleIds:string[]) {
-     try {
-    const res = await serverFetch.post(`/doctor-schedule`, {
-      body: JSON.stringify(scheduleIds),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+export async function createDoctorschedule(scheduleIds: string[]) {
+    try {
+        const res = await serverFetch.post(`/doctor-schedule`, {
+            body: JSON.stringify(scheduleIds),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
 
-    const data = await res.json();
+        const data = await res.json();
 
-   
-  } catch (error: any) {
-    console.error(error);
-    return {
-      success: false,
-      message: `${process.env.NODE_ENV === "development"
-        ? error.message
-        : "Something went wrong"
-        }`,
-    };
-  }
+
+    } catch (error: any) {
+        console.error(error);
+        return {
+            success: false,
+            message: `${process.env.NODE_ENV === "development"
+                ? error.message
+                : "Something went wrong"
+                }`,
+        };
+    }
 }
 
 
-export async function deleteDoctorschedue(scheduleId:string) {
-     try {
-    const res = await serverFetch.delete(`/doctor-schedule/${scheduleId}`);
-    const data = await res.json();
+export async function deleteDoctorOwnschedule(scheduleId: string) {
+    try {
+        const res = await serverFetch.delete(`/doctor-schedule/${scheduleId}`);
+        const data = await res.json();
 
-  return{
-    success:data.success,
-    message:data.message || "Faild to remove schedule"
-  }
-   
-  } catch (error: any) {
-    console.error(error);
-    return {
-      success: false,
-      message: `${process.env.NODE_ENV === "development"
-        ? error.message
-        : "Something went wrong"
-        }`,
-    };
-  }
+        return {
+            success: data.success,
+            message: data.message || "Faild to remove schedule"
+        }
+
+    } catch (error: any) {
+        console.error(error);
+        return {
+            success: false,
+            message: `${process.env.NODE_ENV === "development"
+                ? error.message
+                : "Something went wrong"
+                }`,
+        };
+    }
 }
